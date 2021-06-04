2fa485c8e66f41b162c3bea285f0f9c114b7001e
Danilo Dell'Orco <danilodellorcolp@gmail.com>
2021-06-04 19:31:13 +0200

    jacoco / coverage #4

--- bookkeeper-server/src/test/java/org/apache/bookkeeper/mytests/ProvaLancioTest.java
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/mytests/ProvaLancioTest.java
@@ -3,12 +3,15 @@ package org.apache.bookkeeper.mytests;
 import static org.junit.Assert.assertEquals;
 
 import java.io.IOException;
+import java.util.logging.Level;
 
+import org.apache.bookkeeper.bookie.EntryKey;
 import org.apache.bookkeeper.bookie.LedgerDescriptor;
 import org.apache.bookkeeper.bookie.LedgerDescriptorImpl;
 import org.apache.bookkeeper.bookie.SortedLedgerStorage;
 import org.apache.bookkeeper.client.api.LedgerEntry;
 import org.junit.Test;
+import org.slf4j.Logger;
 
 import com.google.common.primitives.Bytes;
 
@@ -25,9 +28,13 @@ public class ProvaLancioTest {
 	@Test
 	public void dummyTest() throws IOException {
 		SortedLedgerStorage srl = new SortedLedgerStorage();
+		System.out.println(srl);
 		Long id = 4000L;
 		ByteBuf ledger = LedgerDescriptor.createLedgerFenceEntry(id);
-		srl.addEntry(ledger);
+		EntryKey entry = new EntryKey();
+		System.out.println(ledger);
+		System.out.println(entry);
+		
 		int parameter = 5;
 		assertEquals(5, parameter);
 	}
