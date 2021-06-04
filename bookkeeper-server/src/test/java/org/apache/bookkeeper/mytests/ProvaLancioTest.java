feef3fc0d5971fb9809c2690ada27c2884e1be37
Danilo Dell'Orco <danilodellorcolp@gmail.com>
2021-06-04 19:06:29 +0200

    coverage/jacoco #2

--- bookkeeper-server/src/test/java/org/apache/bookkeeper/mytests/ProvaLancioTest.java
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/mytests/ProvaLancioTest.java
@@ -12,6 +12,7 @@ import com.google.common.primitives.Bytes;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.ByteBufUtil;
+import io.netty.buffer.Unpooled;
 
 public class ProvaLancioTest {
 	
@@ -22,8 +23,7 @@ public class ProvaLancioTest {
 	@Test
 	public void dummyTest() throws IOException {
 		SortedLedgerStorage srl = new SortedLedgerStorage();
-		ByteBuf buf = null;
-		ByteBufUtil.writeUtf8(buf, "Entry1");
+		ByteBuf buf = Unpooled.wrappedBuffer(new byte[] { 0x0D, 0X0A });
 		srl.addEntry(buf);
 		int parameter = 5;
 		assertEquals(5, parameter);
