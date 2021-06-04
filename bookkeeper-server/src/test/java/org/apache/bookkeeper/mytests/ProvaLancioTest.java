58196aa547e4e3a111143939f460355501f848a0
Danilo Dell'Orco <danilodellorcolp@gmail.com>
2021-06-04 15:43:20 +0200

    prova build test

---
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/mytests/ProvaLancioTest.java
@@ -0,0 +1,18 @@
+package org.apache.bookkeeper.mytests;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class ProvaLancioTest {
+	
+	public ProvaLancioTest () {
+		// Costruttore
+	}
+	
+	@Test
+	public void dummyTest() {
+		int parameter = 5;
+		assertEquals(5, parameter);
+	}
+}
