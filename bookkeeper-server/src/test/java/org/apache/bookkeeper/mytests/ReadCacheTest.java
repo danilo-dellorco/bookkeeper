1c9be3ae37c1d921eb1826ed9cae95605ab3cf7e
Danilo Dell'Orco <danilodellorcolp@gmail.com>
2021-06-08 15:59:55 +0200

    WriteCache/ Get&Put/ coverage 90%

--- bookkeeper-server/src/test/java/org/apache/bookkeeper/mytests/ReadCacheTest.java
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/mytests/ReadCacheTest.java
@@ -2,68 +2,114 @@ package org.apache.bookkeeper.mytests;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Arrays;
+import java.util.Collection;
+
 import org.apache.bookkeeper.bookie.storage.ldb.ReadCache;
+
+import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.buffer.UnpooledByteBufAllocator;
 
-public class ReadCacheTest {
-	ReadCache cache;
-	ByteBuf entry;
 
-	/*
-	 * Permette di specificare le eccezioni attese
-	 */
+@RunWith(Parameterized.class)
+public class ReadCacheTest {
+	static ReadCache cache;
+	
+	// Cache settings
+	public static int ENTRY_SIZE = 1024;					// Ogni entry viene ha dimensione 1024 byte
+	public static int MAX_ENTRIES = 10;						// Ogni cache può contenere al massimo 10 entry
+	public static int CACHE_SIZE = ENTRY_SIZE*MAX_ENTRIES;	// Massimo numero di byte che può contenere l'entry
+	public static UnpooledByteBufAllocator ALLOCATOR = UnpooledByteBufAllocator.DEFAULT;
+	
+	// Types of entries
+	public static ByteBuf valid_entry;
+	public static ByteBuf illegal_entry;
+	public static ByteBuf null_entry;
+	
+	// Test parameters
+	int ledgerId;
+	int entryId;
+	ByteBuf entry;
+	Class<? extends Exception> expectedException;
+	
+	public ReadCacheTest(int ledgerId, int entryId, ByteBuf entry, Class<? extends Exception> expectedException) {
+		this.ledgerId = ledgerId;
+		this.entryId = entryId;
+		this.entry = entry;
+		this.expectedException = expectedException;
+	}
+
+	// Permette di specificare l'eccezione attesa.
 	@Rule
 	public ExpectedException exceptionRule = ExpectedException.none();
 	
+	
+	// Configuro l'ambiente di esecuzione istanziando una nuova ReadCache prima di ogni test.
 	@Before
 	public void configure() {
-		cache = new ReadCache(UnpooledByteBufAllocator.DEFAULT, 10 * 1024);
-		entry = Unpooled.wrappedBuffer(new byte[1024]);
+		cache = new ReadCache(ALLOCATOR,CACHE_SIZE);
 	}
-
-	public ReadCacheTest() {
+	
+	// Chiudo la cache dopo aver eseguito tutti i test case
+	@AfterClass
+	public static void closeCache() {
+		cache.close();
 	}
-
-	@Test
-	public void initializeReadCacheTest() {
-		assertEquals(0, cache.count());
-		assertEquals(0, cache.size());
-		assertEquals(null, cache.get(0, 0));
-	}
-
+	
+    @Parameters
+    public static Collection<Object[]> data() {
+    	
+		// Setup of the entries
+		valid_entry = Unpooled.wrappedBuffer(new byte[ENTRY_SIZE]);
+		illegal_entry = Unpooled.wrappedBuffer(new byte[CACHE_SIZE+1]);
+		null_entry = null;
+		
+        return Arrays.asList(new Object[][] {
+                {1,0,valid_entry,null},
+                {0,1,illegal_entry,IndexOutOfBoundsException.class},
+                {1,-1,null_entry,NullPointerException.class},
+                {-1,1,valid_entry,IllegalArgumentException.class}
+        });
+    }
+    
+    
+    
 	@Test
 	public void putTest() {
-		cache.put(1, 0, entry); // Inseriamo una entry in cache con LEDGER_ID = 1, ENTRY_ID = 0
-		assertEquals(1, cache.count()); // Vediamo che abbiamo esattamente un'entry
-		assertEquals(1024, cache.size()); // Vediamo che la dimensione totale è data dalla dimensione dell'unica entry
-											// aggiunta
+		System.out.println("______________PUT TEST______________");
+		System.out.println(String.format("ledger: %d\nentry: %d\n", ledgerId,entryId));
+		
+		if (expectedException != null) {
+			exceptionRule.expect(expectedException);
+		}
+		cache.put(ledgerId, entryId, entry);
 
-		// TODO fare ciclo con insieme di entry passate con parametrized
+		assertEquals(1, cache.count()); 				// Vediamo che abbiamo esattamente un'entry
+		assertEquals(ENTRY_SIZE, cache.size()); 		// Vediamo che la dimensione totale è data dalla dimensione dell'unica entry aggiunta
 	}
 
 	@Test
 	public void getTest() {
-		cache.put(1, 0, entry);
+        if(expectedException != null) {
+        	exceptionRule.expect(expectedException);
+        }
+		
+        cache.put(ledgerId, entryId, entry);
 		ByteBuf expectedValue = entry;
-		ByteBuf actualValue = cache.get(1, 0);
-		assertEquals(actualValue, expectedValue);
-	}
-
-	
-
-	// TODO questo non sarà un vero e proprio test ma dovrà diventare risultato atteso quando con parametrized gli passeremo 
-	// ledger id = -1
-	@Test
-	public void exceptionTest() {
-		exceptionRule.expect(IllegalArgumentException.class);
-		cache.put(-1, 0, entry);
+		ByteBuf actualValue = cache.get(ledgerId, entryId);
+		assertEquals(expectedValue, actualValue);			// Verifichiamo che il valore ottenuto dal get è uguale a quello inserito con il put
+		
+		actualValue = cache.get(ledgerId+1, entryId+1);
+		assertEquals(null,actualValue);						// Verifichiamo che il get su un'entry non presente in cache ritorna null
 	}
 }
