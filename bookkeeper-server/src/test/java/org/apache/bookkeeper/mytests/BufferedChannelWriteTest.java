f7ce63db8a53400ee903c54f8ad8edcf56df6091
Danilo Dell'Orco <danilodellorcolp@gmail.com>
2021-06-11 11:37:28 +0200

    Iniziato BufferedChannel Test #1

--- bookkeeper-server/src/test/java/org/apache/bookkeeper/mytests/BufferedChannelWriteTest.java
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/mytests/BufferedChannelWriteTest.java
@@ -3,7 +3,6 @@ package org.apache.bookkeeper.mytests;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.buffer.UnpooledByteBufAllocator;
-import tools.DebugTools;
 
 import org.apache.bookkeeper.bookie.BufferedChannel;
 import org.apache.bookkeeper.mytests.parameters.BufferedChannelWriteParameters;
@@ -35,8 +34,6 @@ import java.util.*;
 @RunWith(Parameterized.class)
 
 public class BufferedChannelWriteTest {
-	
-	private static Random rand = new Random();
 	
 	private static final String TMP_DIR = "tmp/";
 	private static final String LOG_FILE = "BfcWriteFile.log";
@@ -157,7 +154,7 @@ public class BufferedChannelWriteTest {
 		Integer numBytesInFileCh = 0;
 		numBytesInWriteBuff = this.writeBufSize;	// Ottengo il numero di bytes nel buffer di scrittura come la dimensione del writeBuffer
 
-		// Creo un array di bytes per inserire i bytes dentro il buffer
+		// Creo un array di bytes per inserire i bytes dentro il WriteBuffer
 		byte[] bytesInWriteBuf = new byte[numBytesInWriteBuff];
 		
 		// Scrivo i bytes nel WriteBuffer in un array di bytes bytesInWriteBuf
@@ -166,12 +163,15 @@ public class BufferedChannelWriteTest {
 		System.out.println("Bytes presenti nel WriteBuffer: "+Arrays.toString(bytesInWriteBuf));
 		
 
-		// Otengo i byte che dovrebbero essere nel WriteBuf come i bytes random generati togliendo il numero di bytes che sono in writeBuf
+		// Otengo i byte che dovrebbero essere nel WriteBuf come il totale dei bytes random generati togliendo il numero di bytes che sono in writeBuf
 		byte[] expectedBytes = Arrays.copyOfRange(this.bytes, this.bytes.length - numBytesInWriteBuff, this.bytes.length);
 		System.out.println("Bytes attesi che siano nel WriteBuffer: "+Arrays.toString(expectedBytes));
 
 		Assert.assertEquals("Error",Arrays.toString(expectedBytes),Arrays.toString(bytesInWriteBuf));
 
+		
+		
+		
 		ByteBuffer buff = ByteBuffer.allocate(numBytesInFileCh);
 
 		this.fc.position(this.initialPos);
